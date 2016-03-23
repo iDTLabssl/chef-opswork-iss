74b6e87833bb45b2a1699f7c675bd2e702f4d835
Salton Arthur Massally <salton.massally@gmail.com>
2016-03-23 10:36:39 +0000

    update cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'salton.massally@gmail.com'
 license           "Apache 2.0"
 description      'Installs/Configures openerp-server on opswork ... assumes you already have a database running... we use rds'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.7'
+version          '0.3.8'
 
 depends "ohai"
 depends "supervisor"
